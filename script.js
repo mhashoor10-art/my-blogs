fd59bbff16e6ccb2593d5f94c0b26163ff447998
mhashoor10-art <mhashoor10@gmail.com>
2026-03-31 14:20:09 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let blogs = JSON.parse(localStorage.getItem("blogs")) || [];
+
 
 function loadBlogs() {
   let container = document.getElementById("blogList");
@@ -27,3 +27,42 @@ function toggleDark() {
 document.addEventListener("DOMContentLoaded", function () {
   loadBlogs();
 });
+<script type="module">
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBlQP-s0Q-y6J1POkEEHrcDP32Wn6JPK_4",
+  authDomain: "kazim-aa621.firebaseapp.com",
+  projectId: "kazim-aa621",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+async function loadBlogs() {
+  let container = document.getElementById("blogList");
+  container.innerHTML = "";
+
+  const data = await getDocs(collection(db, "blogs"));
+
+  data.forEach((doc) => {
+    let blog = doc.data();
+
+    container.innerHTML += `
+      <div class="card">
+        <a href="post.html?id=${doc.id}">
+          <img src="${blog.img}" style="width:100%; border-radius:10px;">
+          <h3>${blog.title}</h3>
+          <p>${blog.desc}</p>
+          <small>${blog.date}</small>
+        </a>
+      </div>
+    `;
+  });
+}
+
+loadBlogs();
+
+</script>
